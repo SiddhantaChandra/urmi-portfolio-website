867301c183a905a5fae06310ffa11f303836ec3c
Siddhanta Chandra <iamsiddhanta.10@gmail.com>
2025-07-17 15:25:09 +0530

    Added new journalism articles and pictures

--- components/JournalismData.js
+++ components/JournalismData.js
@@ -1,5 +1,117 @@
 const journalArticles = [
     {
+        title: "New anime 'Takopi's Original Sin' is what 'Doraemon' would be in a darker, twisted world",
+        subHeading: "What if Doraemon couldn't save Nobita? What if the blue robot cat, who travelled back in time to rescue a helpless boy with his pocketful of gadgets, arrived too late, or worse, it couldn't comprehend human emotions?",
+        image: "/articles/journalism/takopi.webp",
+        link: "https://www.telegraphindia.com/entertainment/anime-takopis-original-sin-on-crunchyroll-is-the-doraemon-story-you-didnt-know-you-needed/cid/2111159",
+        publication: "Telegraph",
+        category: "Entertainment",
+        articleType: "Published Article",
+        tags: ["Anime", "Takopi's Original Sin", "Doraemon", "Pop Culture"],
+        metrics: [
+            { label: "Publication", value: "Telegraph" },
+            { label: "Category", value: "Entertainment" },
+        ]
+    },
+    {
+        title: "'He taught us to be ourselves': How the craze for 'Shinchan' transcends generations",
+        subHeading: "I caught up with Shinchan fans to reflect on what the anime means to them - now that they've grown up, but didn't quite outgrow the little boy.",
+        image: "/articles/journalism/shinchan.webp",
+        link: "https://www.telegraphindia.com/entertainment/indian-fans-reflect-on-the-nostalgic-appeal-of-shinchan-ahead-of-the-spicy-kasukabe-dancers-release/cid/2108449",
+        publication: "Telegraph",
+        category: "Entertainment",
+        articleType: "Published Article",
+        tags: ['Anime', 'Shinchan', 'Pop Culture', 'Nostalgia'],
+        metrics: [
+            { label: "Publication", value: "Telegraph" },
+            { label: "Category", value: "Entertainment" },
+        ]
+    },
+    {
+        title: "'The Last of Us' S2 to 'Until Dawn': 6 must-watch video game-to-screen adaptations in 2025",
+        subHeading: "Gamers and non-gamers alike are in for a treat this year as several movies based on video games are lined up for release. Here are six game-to-screen adaptations that must be on your watchlist.",
+        image: "/articles/journalism/6_must-watch_video_games_adaptation.webp",
+        link: "https://www.telegraphindia.com/entertainment/6-video-game-to-screen-adaptations-you-mustnt-miss-this-year-photogallery/cid/2080567",
+        publication: "Telegraph",
+        category: "Entertainment",
+        articleType: "Published Article",
+        tags: ['Video games', 'Gaming', 'Movie adaptation', 'Pop Culture'],
+        metrics: [
+            { label: "Publication", value: "Telegraph" },
+            { label: "Category", value: "Entertainment" },
+        ]
+    },
+    {
+        title: "Bong-Pool to topor-clad Spider-Man: A Bengali twist to superheroes at Kolkata Comic Con 2025",
+        subHeading: "The Kolkata edition of Comic Con at Biswa Bangla Mela Prangan saw cosplayers from across the country bring their favourite characters to life over the weekend. But what grabbed eyeballs was the Bengali twist added to fan-favourite superhero characters at the two-day event.",
+        image: "/articles/journalism/cosplay_kolkata_comic_con_2025.webp",
+        link: "https://www.telegraphindia.com/entertainment/cosplayers-bring-valorants-reyna-one-pieces-luffy-and-more-to-life-at-kolkata-comic-con-2025-photogallery/cid/2085366",
+        publication: "Telegraph",
+        category: "Entertainment",
+        articleType: "Published Article",
+        tags: ['Comic Con', 'Kolkata Comic Con', 'Cosplay', 'Pop Culture', 'Anime', 'Comics'],
+        metrics: [
+            { label: "Publication", value: "Telegraph" },
+            { label: "Category", value: "Entertainment" },
+        ]
+    },
+    {
+        title: "7 anime that gave a nod to Hollywood before 'One Piece' referenced viral Willem Dafoe scene",
+        subHeading: "Much before One Piece sparked buzz with its nod to Willem Dafoe's iconic scene from At Eternity’s Gate, many other popular anime referenced iconic scenes from the West. Here's a look.",
+        image: "/articles/journalism/7_anime_that_gave_a_nod_to_hollywood_before_one_piece.webp",
+        link: "https://www.telegraphindia.com/entertainment/7-anime-that-gave-a-nod-to-hollywood-before-one-piece-referenced-viral-willem-dafoe-scene/cid/2110089",
+        publication: "Telegraph",
+        category: "Entertainment",
+        articleType: "Published Article",
+        tags: ['One Piece', 'Anime', 'Pop Culture', 'Gintama', 'Cowboy Bebop', "JoJo's Bizarre Adventure"],
+        metrics: [
+            { label: "Publication", value: "Telegraph" },
+            { label: "Category", value: "Entertainment" },
+        ]
+    },
+    {
+        title: "Kolkata's anime treasure map: Where to find oversized tees, action figures, home decor and more",
+        subHeading: "Whether you're a hardcore fan, a casual enthusiast or someone who just wants to rock a Satoru Gojo t-shirt, Kolkata has you covered with a plethora of anime collectible stores inspired by your favourite shows. Here's a look.",
+        image: "/articles/journalism/kolkatas_anime_treasure_map_where_to_find.webp",
+        link: "https://www.telegraphindia.com/my-kolkata/lifestyle/where-to-buy-anime-t-shirts-action-figures-collectibles-and-merchandise-in-kolkata/cid/2111435",
+        publication: "My Kolkata",
+        category: "Entertainment",
+        articleType: "Published Article",
+        tags: ['Kolkata', 'Anime', 'Anime community', 'City', 'Pop Culture', 'Collectibles'],
+        metrics: [
+            { label: "Publication", value: "Telegraph" },
+            { label: "Category", value: "Entertainment" },
+        ]
+    },
+    {
+        title: "Promise Day: 6 iconic anime love confession scenes that will give you butterflies",
+        subHeading: "From dramatic declarations of love to sweet, innocent gestures that swept us off our feet, here are six iconic love confession scenes in anime that have left a lasting impact on fans.",
+        image: "/articles/journalism/from-kaguya-sama-love-is-war-to-horimiya-and-my-dress-up-darling-6-most-iconic-anime-love-confessions.webp",
+        link: "https://www.telegraphindia.com/entertainment/from-kaguya-sama-love-is-war-to-horimiya-and-my-dress-up-darling-6-most-iconic-anime-love-confessions-to-watch-this-valentines-day-photogallery/cid/2082898",
+        publication: "My Kolkata",
+        category: "Entertainment",
+        articleType: "Published Article",
+        tags: ['Anime', 'Romance anime', 'Romcom', 'Kaguya Sama', 'Horimiya', 'Fruits Basket', 'My Dress-Up Darling'],
+        metrics: [
+            { label: "Publication", value: "Telegraph" },
+            { label: "Category", value: "Entertainment" },
+        ]
+    },
+    {
+        title: "How 'Stranger Things' revived hits by Kate Bush, Metallica before Deep Purple's 'Child In Time'",
+        subHeading: "From Metallica to Kate Bush, Stranger Things has consistently paid tribute to vintage music over the years. As the teaser for Season 5 features Deep Purple's Child in Time, here's how the show revived iconic tracks for a new generation.",
+        image: "/articles/journalism/stranger-things-revived-retro-songs-by-kate-bush-metallica.webp",
+        link: "https://www.telegraphindia.com/entertainment/stranger-things-revived-retro-songs-by-kate-bush-metallica-before-deep-purples-child-in-time/cid/2113709",
+        publication: "My Kolkata",
+        category: "Entertainment",
+        articleType: "Published Article",
+        tags: ['Stranger Things', 'Metallica', 'Deep Purple', 'Kate Bush', 'Rock', 'Metal'],
+        metrics: [
+            { label: "Publication", value: "Telegraph" },
+            { label: "Category", value: "Entertainment" },
+        ]
+    },
+    {
         title: "Harry Potter Day: Does the Wizarding World of J.K. Rowling cast the same spell on Gen Alpha?",
         subHeading: "Explored how the controversies surrounding J.K. Rowling impacted the appeal of Harry Potter among Gen Alpha through interviews with teens and tweens. Developed a multi-platform strategy using my presence on social media to identify and connect with target subjects.",
         image: "/articles/journalism/HarryPotter.webp",
